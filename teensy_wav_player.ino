e49706d77f50dd773b796e0f4dd04e995a0fe1c4
hydronics2 <hydronics@gmail.com>
2015-08-20 08:46:43 -0700

    updated the SD library file SD_t3.h
    PJRC forum topic on buzzing, Paul suggested uncommenting some code in
    SD_t3.h. might also want to update the SD card from class 4 to class 10
    but have not done so yet. buzzing seems to have gone as of the last 24
    hours

--- teensy_wav_player.ino
+++ teensy_wav_player.ino
@@ -3,10 +3,8 @@
 // when pin 2 is pulled LOW, the sound is played "water.wav"
 // the audio file needs to be 16bit, 44.1khz, 
 
-
-
-
-
+// 8/17/15  - teensy was generating a loud sound. took out one of the while loops in play.file. added a coupld of 20ms delays, changed volume to 0.9
+// 8/19/15 - changed library SD_t3.h (uncommenting out USE_TEENsy3_OPTIMIZED_CODE) basd on pjrc forum topic
 
 //
 // Requires the audio shield:
@@ -32,14 +30,14 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=240,153
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);
-  digitalWrite(2, HIGH);
+  pinMode(2, INPUT);
+  //digitalWrite(2, HIGH);
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
-  AudioMemory(5);
+  AudioMemory(50);
 
   sgtl5000_1.enable();
-  sgtl5000_1.volume(.5);
+  sgtl5000_1.volume(.8);
 
   SPI.setMOSI(7);
   SPI.setSCK(14);
@@ -64,12 +62,6 @@ void playFile(const char *filename)
   // A brief delay for the library read WAV info
   delay(5);
 
-  // Simply wait for the file to finish playing.
-//  while (playWav1.isPlaying()) {
-  
-    //while (digitalRead(2) == LOW) {
-    //delay(20);
-  //}
 }
 
 
@@ -87,5 +79,8 @@ void loop() {
 
   if (digitalRead(2) == HIGH) {
   playWav1.stop();
-}}
+}
 
+delay(20);
+
+}
